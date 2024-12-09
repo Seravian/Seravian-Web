13c88f8b9ae237b506ef35b9284138485210d76c
Khalidsaied <115695838+Khalidsaied@users.noreply.github.com>
2024-12-09 23:45:53 +0200

    organize

--- src/app/component/dashboard/dashboard.module.ts
+++ src/app/component/dashboard/dashboard.module.ts
@@ -4,8 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeBodyComponent } from './home-body/home-body.component';
-import { TeamMemberComponent } from './team-member/team-member.component';
+import { TestimonialSectionComponent } from './home/main-content/testimonial-section/testimonial-section.component';
+import { AboutUsComponent } from './home/main-content/about-section/about-us/about-us.component';
+import { TeamMemberComponent } from './home/main-content/about-section/team-member/team-member.component';
+import { MainContentComponent } from './home/main-content/main-content.component';
 import { HeaderComponent } from './header/header.component';
 
 
@@ -13,10 +15,11 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     HomeComponent,
     FooterComponent,
-    HomeBodyComponent,
-    TeamMemberComponent,
     HeaderComponent,
-
+    TestimonialSectionComponent,
+    AboutUsComponent,
+    TeamMemberComponent,
+    MainContentComponent,
   ],
   imports: [
     CommonModule,
