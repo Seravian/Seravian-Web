b8e3dab83a85a93b6b96198e5b4fd65d6802ac06
alhrery <aboodyalhrery@gmail.com>
2025-06-16 06:59:14 +0300

    final changes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-
+console.log = () => {};
+console.error = () => {};
+console.warn = () => {};
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { register } from 'swiper/element/bundle';
