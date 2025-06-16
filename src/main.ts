b2688b41af750a3436a9ebdad3f8d31742df17e9
alhrery <aboodyalhrery@gmail.com>
2025-06-16 06:49:09 +0300

    buggggggggggg email

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-console.log = () => {};
-console.error = () => {};
-console.warn = () => {};
+
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { register } from 'swiper/element/bundle';
